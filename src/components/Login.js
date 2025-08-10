803ad812d2612381245be250699b754f7026e6d4
mohsinshah12309 <mohsinalishahnaqvi123@gmail.com>
2025-08-10 21:23:21 +0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,7 +23,7 @@ const Login = (props) => {
         throw new Error("Please fill in all fields");
       }
 
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://my-notebook-backend-green.vercel.app/api/auth/login", {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
